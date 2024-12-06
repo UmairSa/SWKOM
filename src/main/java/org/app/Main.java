add276abb7e19effd2b6dc92f3bd4375114a0684
if22b179 <if22b179@technikum-wien.at>
2024-12-06 12:27:20 +0100

    bugfix

--- src/main/java/org/app/Main.java
+++ src/main/java/org/app/Main.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
