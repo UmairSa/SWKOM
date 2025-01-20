cae7eca5029dfc8a0a5ef4c7671ffffbcb809a38
UmairSa <umairsaqub@gmail.com>
2025-01-20 13:09:17 +0100

    logging, validation and lombok

--- src/main/java/org/app/dal/entity/DocumentEntity.java
+++ src/main/java/org/app/dal/entity/DocumentEntity.java
@@ -1,12 +1,13 @@
 package org.app.dal.entity;
 
-
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.time.LocalDateTime;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DocumentEntity {
 
     @Id
@@ -17,47 +18,4 @@ public class DocumentEntity {
 
     @Column(name = "date_uploaded", nullable = false, updatable = false)
     private LocalDateTime dateUploaded;
-
-
-    public DocumentEntity(Long id, String name, String content, LocalDateTime dateUploaded) {
-        this.id = id;
-        this.name = name;
-        this.content = content;
-        this.dateUploaded = dateUploaded;
-    }
-
-    public DocumentEntity() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getDateUploaded() {
-        return dateUploaded;
-    }
-
-    public void setDateUploaded(LocalDateTime dateUploaded) {
-        this.dateUploaded = dateUploaded;
-    }
 }
--- src/main/java/org/app/dto/DocumentDto.java
+++ src/main/java/org/app/dto/DocumentDto.java
@@ -2,63 +2,19 @@ package org.app.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import java.time.LocalDateTime;
 import lombok.*;
 
-import java.time.LocalDateTime;
-
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DocumentDto {
     private Long id;
 
-    @NotBlank(message = "Name must not be blank")
-    @Size(max = 100, message = "Name must not exceed 100 characters")
+    @NotBlank(message = "Name cannot be empty!")
+    @Size(max = 100, message = "Name cannot exceed 100 characters!")
     private String name;
 
     private String content;
     private LocalDateTime dateUploaded;
-
-
-
-    public DocumentDto() {
-    }
-
-    public DocumentDto(Long id, String name, String content, LocalDateTime dateUploaded) {
-        this.id = id;
-        this.name = name;
-        this.content = content;
-        this.dateUploaded = dateUploaded;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public @NotBlank(message = "Name must not be blank") @Size(max = 100, message = "Name must not exceed 100 characters") String getName() {
-        return name;
-    }
-
-    public void setName(@NotBlank(message = "Name must not be blank") @Size(max = 100, message = "Name must not exceed 100 characters") String name) {
-        this.name = name;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getDateUploaded() {
-        return dateUploaded;
-    }
-
-    public void setDateUploaded(LocalDateTime dateUploaded) {
-        this.dateUploaded = dateUploaded;
-    }
 }
